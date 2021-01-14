011ffdaa9a5fa1446cece4b24a931305085c0865
GowthamF <40605124+GowthamF@users.noreply.github.com>
2021-01-14 23:20:26 +0530

    Learning

--- Node-First-App/app.js
+++ Node-First-App/app.js
@@ -1,6 +1,13 @@
-const http = require('http');
-const routes = require('./routes')
+const express = require('express');
 
-const server = http.createServer(routes.requestHandler);
+const app = express();
 
-server.listen(3001);
+app.use('/add-product',(req,res,next)=>{
+    res.send('<h1>Add Product</h1>');
+});
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello from Express</h1>');
+});
+
+app.listen(3000);
