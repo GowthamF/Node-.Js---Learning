6ee3e07d48a537a3d81eda491c6e6188b0b596a0
GowthamF <40605124+GowthamF@users.noreply.github.com>
2021-01-21 19:54:15 +0530

    Implmenting dynamic route

---
+++ 09-dynamic_route/models/product.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const path = require('path');
+
+const Cart = require('./cart');
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id=id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    
+    
+    getProductsFromFile(products => {
+      if(this.id){
+        const existingProductIndex = products.findIndex(prod=> prod.id === this.id);
+        const updatedProduct = [...products];
+        updatedProduct[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProduct), err => {
+          console.log(err);
+        });
+      }else{
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
+      
+      
+    });
+  }
+
+  static deleteProduct(id){
+    getProductsFromFile(products=>{
+      // This is also another way
+      // const deletedProductIndex = products.findIndex(prod=> prod.id === id);
+      // const updatedProduct = [...products];
+      // updatedProduct.splice(deletedProductIndex,1);
+
+      //Easiest and Simple one
+      const product = products.find(prod=> prod.id === id);
+      const updatedProduct = products.filter(prod=> prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProduct), err => {
+        if(!err){
+          Cart.deleteCartItem(product.id,product.price);
+        }
+        console.log(err);
+      });
+    })
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id,cb){
+    getProductsFromFile((products)=>{
+      const product = products.find(c=>c.id === id);
+      cb(product);
+    });
+  }
+};
