aac3c7f6441815012b4da2d3553bbf401b95d5e5
GowthamF <40605124+GowthamF@users.noreply.github.com>
2021-01-31 23:48:07 +0530

    Implementing Mongoose

---
+++ 13-Mongoose/models/product.js
@@ -0,0 +1,151 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    title:{
+        type: String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    description:{
+        type: String,
+        required:true
+    },
+    imageUrl:{
+        type: String,
+        required:true
+    },
+    userId:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+
+
+
+module.exports = mongoose.model('Product',ProductSchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const getDb = require('../util/database').getDb;
+// const ObjectId = require('mongodb').ObjectId;
+
+// class Product{
+//     constructor(title,price,imageUrl,description, id, userId){
+//         this.title = title;
+//         this.price = price;
+//         this.imageUrl = imageUrl;
+//         this.description = description;
+//         this._id = id ? new ObjectId.createFromHexString(id) : null;
+//         this.userId = userId;
+//     }
+
+//     save(){
+//         const db = getDb();
+//         let dbOp;
+//         if(this._id){
+//             dbOp = db.collection('products').updateOne(
+//                 {_id:this._id},
+//                 {
+//                     // $set:{
+//                     //     title: this.title,
+//                     //     price : this.price,
+//                     //     imageUrl : this.imageUrl,
+//                     //     description : this.description
+//                     // }
+//                     $set : this
+//                 });
+//         }else{
+//             dbOp = db.collection('products')
+//             .insertOne(this);
+//         }
+
+//         return dbOp
+//             .then((result)=>{
+//                 console.log(result);
+//             })
+//             .catch((err)=>{
+//                 console.log(err);
+//             });
+        
+//     }
+
+//     static fetchAll(){
+//         const db = getDb();
+//         return db.collection('products').find().toArray().then((products)=>{
+//             console.log(products);
+//             return products;
+//         }).catch((err)=>{
+//             console.log(err);
+//         })
+//     }
+
+//     static findById(prodId){
+//         const db = getDb();
+//         console.log(prodId);
+//         return db.collection('products').findOne({_id: new ObjectId.createFromHexString(prodId)}).then((product)=>{
+//             console.log(product);
+//             return product;
+//         }).catch((err)=>{
+//             console.log(err);
+//         })
+//     }
+
+//     static deleteById(prodId){
+//         const db = getDb();
+
+//         return db.collection('products').deleteOne({_id : new ObjectId.createFromHexString(prodId)});
+//     }
+// }
+
+// module.exports = Product;
